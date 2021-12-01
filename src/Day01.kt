290405cb2a14537daa7e42f832a85792d5c95971
Paul Matthews <pmatthews@google.com>
2021-12-01 21:49:09 +0000

    Completing Day 01 Challenge
    * Created part1 to solve the Day 01 challenge
    * Created part2 to solve the Day 01 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,11 +1,13 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+typealias IntPair = Pair<Int, Int>
+fun List<String>.toInts(): List<Int> = map{ it.toInt() }
+fun Iterable<IntPair>.countIncreasing() = count {(a, b) -> b > a}
+fun main() {
+    fun part1(input: List<String>): Int =
+        input.toInts().zipWithNext().countIncreasing()
+
+    fun part2(input: List<String>): Int =
+        input.toInts().windowed(3).zipWithNext {a, b -> IntPair(a.sum(), b.sum()) }.countIncreasing()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
