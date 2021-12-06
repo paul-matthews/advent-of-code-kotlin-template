b14588e84cd74e1b338894980f8706462072bb25
Paul Matthews <pmatthews@google.com>
2021-12-06 20:25:10 +0000

    Fixing day05 code
    * Solved part2 of Day05

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.math.sign
 
 val VENT_LINE_FORMAT = """^(\d+),(\d+)\s*->\s*(\d+),(\d+)$""".toRegex()
 typealias Coordinate = Pair<Int, Int>
@@ -22,6 +23,16 @@ fun List<String>.toNearbyVents(allowDiagonal: Boolean = false): List<Line> =
         acc
     }
 typealias OverlapMap = Array<Array<Int>>
+fun OverlapMap.get(coord: Coordinate) = get(coord.second)[coord.first]
+fun OverlapMap.set(coord: Coordinate, value: Int) {
+    get(coord.second)[coord.first] = value
+}
+
+fun OverlapMap.println() = map {
+    println(it.joinToString())
+    this
+}
+
 typealias HydrothermalMap = Pair</* Lines */ List<Line>, /* Matches */ OverlapMap>
 fun List<Line>.createMap(): HydrothermalMap {
     val coords = map { it.first } + map { it.second }
@@ -29,30 +40,31 @@ fun List<Line>.createMap(): HydrothermalMap {
     val maxY = coords.maxOf { it.second }
     return HydrothermalMap(
         this,
-        Array(maxX + 1) { Array(maxY + 1) { 0 } }
+        Array(maxY + 1) { Array(maxX + 1) { 0 } }
     )
 }
 fun OverlapMap.markLine(line: Line): OverlapMap {
+    val markings = clone()
     val start = line.first
     val end = line.second
-    val (startX, endX) = if (start.first < end.first) Pair(start.first, end.first) else Pair(end.first, start.first)
-    val (startY, endY) = if (start.second < end.second) Pair(start.second, end.second) else Pair(end.second, start.second)
-
-    val markings = clone()
-    (startX..endX).map {x ->
-        (startY..endY).map {y ->
-            markings.apply {
-                get(x)[y] = get(x)[y] + 1
-            }
+    val incX = sign((end.first - start.first).toDouble()).toInt()
+    val incY = sign((end.second - start.second).toDouble()).toInt()
+    var curX = start.first
+    var curY = start.second
+    while (true) {
+        markings.apply {
+            get(curY)[curX] = get(curY)[curX] + 1
         }
+        if (curX == end.first && curY == end.second)
+            break
+        curX += incX
+        curY += incY
     }
     return markings
 }
 fun HydrothermalMap.getOverlapPoints(): List<Int> {
     var markings = second
     first.map { markings = second.markLine(it) }
-    markings.map {
-    }
     return markings.flatten()
 }
 
@@ -64,17 +76,21 @@ fun main() {
             .getOverlapPoints()
             .count { it > 1 }
 
-//    fun part2(input: List<String>): Int =
-//        input.size
+    fun part2(input: List<String>): Int =
+        input
+            .toNearbyVents(true)
+            .createMap()
+            .getOverlapPoints()
+            .count { it > 1 }
 
     val testInput = readInput("Day05_test")
     val part1Result = part1(testInput)
     check(part1Result == 5) { "Expected: 5 but found $part1Result" }
 
-//    val part2Result = part2(testInput)
-//    check(part2Result == 900, { "Expected 900 but is: $part2Result"})
+    val part2Result = part2(testInput)
+    check((part2Result) == 12) { "Expected 12 but is: $part2Result" }
 
     val input = readInput("Day05")
     println("Part1: " + part1(input))
-//    println("Part2: " + part2(input))
+    println("Part2: " + part2(input))
 }
